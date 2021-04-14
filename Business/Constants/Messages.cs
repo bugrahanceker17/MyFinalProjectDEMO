319ce7add53878f73df9011a0ed494a7a18f4f53
Bugrahan Ceker <bugrahanceker17@outlook.com.tr>
2021-04-15 02:56:03 +0300

    14.gün sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
+using Core.Entities.Concrete;
 using Entities.Concrete;
 
 namespace Business.Constants
@@ -14,5 +16,11 @@ namespace Business.Constants
         public static string ProductCountOFCategoryError="Belirtilen kategori ürün sınırını aştı.";
         public static string ProductNameAlreadyExists = "Aynı isimde zaten ürün var.";
         public static string CategoryIsFull="15'ten fazla kategori eklenemez.";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserRegistered = "Kullanıcı kaydı başarılı";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Hatalı parola";
+        public static string SuccessfulLogin = "Giriş başarılı";
+        public static string UserAlreadyExists = "Kullanıcı zaten var";
     }
 }
